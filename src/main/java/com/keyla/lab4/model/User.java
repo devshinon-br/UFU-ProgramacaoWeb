72d651e752aa08ffb8daae31009f95ea75087598
devshinon-br <rezendekeyla126@gmail.com>
2022-01-24 19:22:42 -0300

    Initial Commit

---
+++ src/main/java/com/keyla/lab4/model/User.java
@@ -0,0 +1,43 @@
+package com.keyla.lab4.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
